b293d9eff7ec7a35de487ef274d73e0618eb41c3
parth adhikari <parthadhikari1812@gmail.com>
2024-02-13 19:04:58 +0530

    made model

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    email = models.EmailField(max_length=254)
+    name = models.CharField(max_length=122)
+    address = models.CharField(max_length=12)
+    city = models.CharField(max_length=122)
+    zip = models.IntegerField()
