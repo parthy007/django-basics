da72673d513ba81e746bbfb11a5b84a4aa56f159
parth adhikari <parthadhikari1812@gmail.com>
2024-02-16 18:04:10 +0530

    adding auth functions

--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from Home.models import Contact
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
     return render(request,"index.html")
 
 def about(request):
@@ -21,3 +25,19 @@ def contact(request):
         contact.save()
         messages.success(request, "Your form is saved!!")
     return render(request,"contact.html")
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:   
+            login(request,user)
+            return redirect('/')
+        else:
+            return render(request,'login.html')
+    return render(request,'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/login')
